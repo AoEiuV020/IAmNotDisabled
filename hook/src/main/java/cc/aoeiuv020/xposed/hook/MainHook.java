c48e65aa3d0ed9ad758d60bf0802f07fe4acf0f4
AoEiuV020 <aoeiuv020@gmail.com>
2022-01-06 03:28:51 +0800

    优化hook安装包体积
    放弃kotlin，改用java，

---
+++ hook/src/main/java/cc/aoeiuv020/xposed/hook/MainHook.java
@@ -0,0 +1,26 @@
+package cc.aoeiuv020.xposed.hook;
+
+import java.util.Collections;
+
+import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+
+/**
+ * Created by AoEiuV020 on 2022.01.06-03:25:32.
+ */
+@SuppressWarnings("RedundantThrows")
+public class MainHook implements IXposedHookZygoteInit {
+    @Override
+    public void initZygote(StartupParam startupParam) throws Throwable {
+        XposedBridge.log("initZygote: " + startupParam.modulePath);
+        XposedHelpers.findAndHookMethod(
+                "android.view.accessibility.AccessibilityManager",
+                null,
+                "getEnabledAccessibilityServiceList",
+                int.class,
+                XC_MethodReplacement.returnConstant(Collections.emptyList())
+        );
+    }
+}
