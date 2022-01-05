7df4b2eac837feac5998cf36dfe4a18629934ea4
AoEiuV020 <aoeiuv020@gmail.com>
2022-01-06 02:30:46 +0800

    实现hook假装自己没有使用无障碍服务

--- app/src/main/java/cc/aoeiuv020/iamnotdisabled/MainActivity.kt
+++ app/src/main/java/cc/aoeiuv020/iamnotdisabled/MainActivity.kt
@@ -1,11 +1,40 @@
 package cc.aoeiuv020.iamnotdisabled
 
+import android.accessibilityservice.AccessibilityServiceInfo
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.accessibility.AccessibilityManager
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkDisabled()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun checkDisabled() {
+        val tvResult: TextView = findViewById(R.id.tvResult)
+        val tvReason: TextView = findViewById(R.id.tvReason)
+
+        val accessibilityManager =
+            ContextCompat.getSystemService(this, AccessibilityManager::class.java)
+                ?: error("unreachable")
+        val serviceList: List<AccessibilityServiceInfo> =
+            accessibilityManager.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_ALL_MASK)
+        if (serviceList.isNotEmpty()) {
+            tvResult.text = "你是残疾人！"
+            tvReason.text = "因为你正在使用：\n" + serviceList.joinToString("\n") {
+                packageManager.getApplicationLabel(it.resolveInfo.serviceInfo.applicationInfo)
+            }
+        } else {
+            tvResult.text = "你很健康"
+            tvReason.text = ""
+        }
     }
 }
