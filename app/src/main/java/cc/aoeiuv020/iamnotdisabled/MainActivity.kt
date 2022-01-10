bc9a7b4e94e81b89f434be94b94a1d4c6a555b0f
AoEiuV020 <aoeiuv020@gmail.com>
2022-01-10 21:33:54 +0800

    添加针对ENABLED_ACCESSIBILITY_SERVICES的处理

--- app/src/main/java/cc/aoeiuv020/iamnotdisabled/MainActivity.kt
+++ app/src/main/java/cc/aoeiuv020/iamnotdisabled/MainActivity.kt
@@ -2,11 +2,12 @@ package cc.aoeiuv020.iamnotdisabled
 
 import android.accessibilityservice.AccessibilityServiceInfo
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.provider.Settings
+import android.view.View
 import android.view.accessibility.AccessibilityManager
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +15,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        findViewById<View>(R.id.tvResult).setOnClickListener {
+            checkDisabled()
+        }
         checkDisabled()
     }
 
@@ -22,19 +26,38 @@ class MainActivity : AppCompatActivity() {
         val tvResult: TextView = findViewById(R.id.tvResult)
         val tvReason: TextView = findViewById(R.id.tvReason)
 
+        val serviceList = getFromAccessibilityManager() + getFromSettingsSecure()
+        if (serviceList.isNotEmpty()) {
+            tvResult.text = "你是残疾人！"
+            tvReason.text = "因为你正在使用：\n" + serviceList.joinToString("\n")
+        } else {
+            tvResult.text = "你很健康"
+            tvReason.text = ""
+        }
+    }
+
+    private fun getFromAccessibilityManager(): List<String> {
         val accessibilityManager =
             ContextCompat.getSystemService(this, AccessibilityManager::class.java)
                 ?: error("unreachable")
         val serviceList: List<AccessibilityServiceInfo> =
             accessibilityManager.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_ALL_MASK)
-        if (serviceList.isNotEmpty()) {
-            tvResult.text = "你是残疾人！"
-            tvReason.text = "因为你正在使用：\n" + serviceList.joinToString("\n") {
-                packageManager.getApplicationLabel(it.resolveInfo.serviceInfo.applicationInfo)
-            }
+                ?: emptyList()
+        return serviceList.map {
+            packageManager.getApplicationLabel(it.resolveInfo.serviceInfo.applicationInfo)
+                .toString()
+        }
+    }
+
+    private fun getFromSettingsSecure(): List<String> {
+        val settingValue = Settings.Secure.getString(
+            contentResolver,
+            Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES
+        )
+        return if (settingValue.isEmpty()) {
+            emptyList()
         } else {
-            tvResult.text = "你很健康"
-            tvReason.text = ""
+            settingValue.split(':')
         }
     }
 }
--- hook/src/main/java/cc/aoeiuv020/iamnotdisabled/hook/MainHook.java
+++ hook/src/main/java/cc/aoeiuv020/iamnotdisabled/hook/MainHook.java
@@ -1,17 +1,33 @@
 package cc.aoeiuv020.iamnotdisabled.hook;
 
+import android.content.ContentResolver;
+
 import java.util.Collections;
 
+import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 /**
  * Created by AoEiuV020 on 2022.01.06-03:25:32.
  */
 @SuppressWarnings("RedundantThrows")
-public class MainHook implements IXposedHookZygoteInit {
+public class MainHook implements IXposedHookLoadPackage, IXposedHookZygoteInit {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        XposedHelpers.findAndHookMethod("android.provider.Settings$Secure", lpparam.classLoader, "getString", ContentResolver.class, String.class, new XC_MethodHook() {
+            protected void beforeHookedMethod(MethodHookParam methodHookParam2) throws Throwable {
+                if ("enabled_accessibility_services".equals((String) methodHookParam2.args[1])) {
+                    methodHookParam2.setResult("");
+                }
+            }
+        });
+    }
+
     @Override
     public void initZygote(StartupParam startupParam) throws Throwable {
         XposedBridge.log("initZygote: " + startupParam.modulePath);
